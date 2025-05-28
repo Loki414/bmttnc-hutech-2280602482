d24b997f9f6df505a3cf741d300b757095da0947
Minakirium <nhatnguyenminh010203@gmail.com>
2025-05-28 10:39:19 +0700

    [add] lab-02 transposition

--- lab-02/api.py
+++ lab-02/api.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from cipher.caesar import CaesarCipher # Giả định rằng bạn có một mô-đun tên là 'cipher' với một tệp 'caesar.py' chứa lớp CaesarCipher
 from cipher.vigenere import VigenereCipher
 from cipher.railfence import RailFenceCipher
-
+from cipher.transposition import TranspositionCipher
 app = Flask(__name__)
 
 # CAESAR CIPHER ALGORITHM
@@ -61,6 +61,26 @@ def decrypt():
     cipher_text = data['cipher_text']
     key = int(data['key'])
     decrypted_text = railfence_cipher.rail_fence_decrypt(cipher_text, key)
+    return jsonify({'decrypted_text': decrypted_text})
+
+# Thêm đoạn sau vào trước hàm main
+#TRANSPOSITION CIPHER ALGORITHM
+transposition_cipher = TranspositionCipher()
+
+@app.route('/api/transposition/encrypt', methods=['POST'])
+def transposition_encrypt():
+    data = request.get_json()
+    plain_text = data.get('plain_text')
+    key = int(data.get('key'))
+    encrypted_text = transposition_cipher.encrypt(plain_text, key)
+    return jsonify({'encrypted_text': encrypted_text})
+
+@app.route('/api/transposition/decrypt', methods=['POST'])
+def transposition_decrypt():
+    data = request.get_json()
+    cipher_text = data.get('cipher_text')
+    key = int(data.get('key'))
+    decrypted_text = transposition_cipher.decrypt(cipher_text, key)
     return jsonify({'decrypted_text': decrypted_text})
 
 #main function
